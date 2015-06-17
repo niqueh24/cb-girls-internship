f92fed7b4502dd77c14f24a33009c880ae27b078
niqueh24 <niquehuff1@gmail.com>
2015-06-17 11:38:38 -0400

    change in database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150617145407) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "t_thoughts", force: :cascade do |t|
     t.text     "nique"
     t.datetime "created_at", null: false
